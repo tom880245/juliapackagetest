fa3b060ff60a19df05611c6be88533114316a2c9
tom880245 <40041106s@gmail.com>
2024-06-23 15:39:43 +0800

    Package commit 0623 4 test

--- src/MyPkg.jl
+++ src/MyPkg.jl
@@ -1,5 +1,22 @@
 module MyPkg
-
+export add, to_uppercase
 # Write your package code here.
+
+function add(x, y)
+    return x + y
+end
+
+function to_uppercase(s::String)
+    return uppercase(s)
+end
+
+function divide(x, y)
+    if y == 0
+        throw(ArgumentError("Division by zero"))
+    end
+    return x / y
+end
+
+
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,7 @@
 using MyPkg
 using Test
-
 @testset "MyPkg.jl" begin
-    # Write your tests here.
+    @test add(2, 3) == 5
+    @test to_uppercase("hello") == "HELLO"
+    @test_throws UndefVarError MyPackage.divide(1, 0)
 end
